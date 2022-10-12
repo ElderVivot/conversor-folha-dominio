1a1b04d549280c7bd2a5ba7c2b9a0766f2b55abc
ElderVivot <eldervivot@gmail.com>
2022-10-12 18:44:46 -0300

    fix: update do dont generate fotrocas_cargos when codi_emp is differente of codi_emp_cargos

--- src/sqls/filter_companie/9_fotrocas_cargos.sql
+++ src/sqls/filter_companie/9_fotrocas_cargos.sql
@@ -6,5 +6,6 @@ SELECT IsNull( String( bethadba.fotrocas_cargos.codi_emp), 'NULO'),
     FROM bethadba.fotrocas_cargos
     
    WHERE bethadba.fotrocas_cargos.codi_emp = '#codi_emp#'
+     AND ( bethadba.fotrocas_cargos.codi_emp = bethadba.fotrocas_cargos.codi_emp_cargos OR bethadba.fotrocas_cargos.codi_emp_cargos IS NULL )
    
 ORDER BY 1, 2
