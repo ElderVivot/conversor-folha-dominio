8877525d5029dadbca7baaa948f408910994b31f
ElderVivot <eldervivot@gmail.com>
2022-10-12 08:29:47 -0300

    fix: update name field to i_jornada

--- src/sqls/filter_companie/9_fotrocas_jornadas.sql
+++ src/sqls/filter_companie/9_fotrocas_jornadas.sql
@@ -1,6 +1,6 @@
 SELECT IsNull( String( bethadba.fotrocas_jornadas.codi_emp), 'NULO'),
          IsNull( String( bethadba.fotrocas_jornadas.i_empregados), 'NULO'), 
-         IsNull( String( bethadba.fotrocas_jornadas.i_jornadas), 'NULO'), 
+         IsNull( String( bethadba.fotrocas_jornadas.i_jornada), 'NULO'), 
          IsNull( String( bethadba.fotrocas_jornadas.data_troca), 'NULO')
 
     FROM bethadba.fotrocas_jornadas
